6b409c68524045cbf6a97429eaa7b3bb37abdf90
Iván Coronel <imc8046@gmail.com>
2026-06-21 19:12:49 -0300

    Fix test

--- src/test/java/com/tallerwebi/presentacion/ControladorLoginTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorLoginTest.java
@@ -65,7 +65,7 @@ public class ControladorLoginTest {
     ModelAndView modelAndView = controladorLogin.validarLogin(datosLoginMock, requestMock);
 
     // validacion
-    assertThat(modelAndView.getViewName(), equalToIgnoringCase("redirect:/home"));
+    assertThat(modelAndView.getViewName(), equalToIgnoringCase("redirect:/bienvenida"));
     verify(sessionMock, times(1)).setAttribute("ROL", usuarioEncontradoMock.getRol());
   }
 
